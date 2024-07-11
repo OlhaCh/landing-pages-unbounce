da32d226832cad5718d60c61268807f987f41e9c
Olha <ochebotarova@havenhealthmgmt.org>
2024-07-11 10:22:05 -0400

    Initial commit with landing pages

---
+++ unbranded-inpatient/unbranded-inpatient-1/scripts/main.js
@@ -0,0 +1,9 @@
+//Slider settings
+$(".slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    prevArrow: $('.slider__prev'),
+    nextArrow: $('.slider__next'),
+});
